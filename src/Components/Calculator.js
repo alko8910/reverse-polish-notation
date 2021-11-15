453de8ed6fd0f055b48f9141aa2b03f56cf40933
alko8910 <almedinemkic@gmail.com>
2021-11-15 21:00:41 +0100

    final version

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -5,87 +5,82 @@ import React, { useState } from 'react';
 function Calculator() {
 
     const [input, setInput] = useState([''])
-    const [message,setMessage] = useState('');
+    const [message, setMessage] = useState('');
     const [color, setColor] = useState('white');
-    const [textColor, setTextColor]= useState('#4097fb')
+    const [textColor, setTextColor] = useState('#4097fb')
     let stack = [];
     console.log(stack)
     let arr = [];
-   // let operator = [];
+    // let operator = [];
 
     const compute = () => {
-        if(input.length === 0){
+        if (input.length === 0) {
             return setMessage('You did not enter antyhing!')
-        }else{
-            arr = input.toString().split(',');
+        } else {
+            arr = input.split(',');
         }
-
-
-
-        /*
-        for(var i = 0; i < arr.length; i++){
-            if ( typeof (arr[i] === 'number') || arr[i] == '/' || arr[i] == '*' || arr[i] == '+' || arr[i] == '-'){
-                
-            }
-        }*/
-        
-        
-        
-        console.log(arr)
-        for(var i = 0; i < arr.length; i++){
+           console.log(arr)
+        for (var i = 0; i < arr.length; i++) {
             // converts string to number and push to stack 
             stack.push(parseFloat(arr[i]));
             // remove NaN from stack
-            stack = stack.filter( value => !Number.isNaN(value));
-         
-    }
-    
-    console.log(stack.length)
-    console.log(arr.length)
-if(arr.some(elem => !['+', '-','/','*','0','1','2','3','4','5','6','7','8','9','.'].includes(String(elem)))) {
-    setMessage('Only numbers and arithmetic operators are valid')
-    
-} else {
-    if(stack.length * 2 === arr.length + 1){
-        for(var j = 0; j < arr.length; j++) {
-            if(arr[j] === "+" || arr[j] === '-' || arr[j] === '/' || arr[j] === '*' ){
-                let b = stack.pop();
-                let a = stack.pop();
-             
-                let oper = arr[j];
-                switch (oper) {
-                    case '+':
-                        stack.push(b+a)
-                        break;
-                    case '-':
-                        stack.push(b-a)
-                        break;
-                    case '/':
-                        stack.push(b/a)
-                        break;
-                    case '*':
-                        stack.push(b*a)
-                        break;
-                        default:
-                }
-                
-        }
-        console.log(stack)
-  
-        setColor('#4097fb');
-        setTextColor('white')
-        setInput(`${input} = ${stack}`)
-        setMessage('')
+            stack = stack.filter(value => !Number.isNaN(value));
 
-        }    
-    } else if(stack.length * 2 > arr.length + 1) {
-        setMessage('Something went wrong! Check the number of operands!')
-    } else {
-        setMessage('Something went wrong! Check the number of operators!')
+        }
+
+        const isValidElement = (elem) => {
+            if (["+", "-", "*", "/"].includes(elem)) {
+                return true;
+            }
+
+            return !Number.isNaN(Number(elem));
+        }
+
+    
+        if (arr.some(elem => !isValidElement(elem))) {
+            setMessage('Only numbers and arithmetic operators are valid')
+
+            } else {
+            if (stack.length * 2 === arr.length + 1) {
+                for (let j = 0; j < arr.length; j++) {
+                    if (arr[j] === "+" || arr[j] === '-' || arr[j] === '/' || arr[j] === '*') {
+                        const b = stack.pop();
+                        const a = stack.pop();
+
+                        const oper = arr[j];
+                        switch (oper) {
+                            case '+':
+                                stack.push(b + a)
+                                break;
+                            case '-':
+                                stack.push(b - a)
+                                break;
+                            case '/':
+                                stack.push(b / a)
+                                break;
+                            case '*':
+                                stack.push(b * a)
+                                break;
+                            default:
+                        }
+
+                    }
+                    console.log(stack)
+
+                    setColor('#4097fb');
+                    setTextColor('white')
+                    setInput(`${input} = ${stack[0].toFixed(2)}`)
+                    setMessage('')
+
+                }
+            } else if (stack.length * 2 > arr.length + 1) {
+                setMessage('Something went wrong! Check the number of operands!')
+            } else {
+                setMessage('Something went wrong! Check the number of operators!')
+            }
+        }
+
     }
-    }
-  
-}
 
     const clear = () => {
         setInput('');
@@ -94,18 +89,20 @@ if(arr.some(elem => !['+', '-','/','*','0','1','2','3','4','5','6','7','8','9','
         setTextColor('#4097fb');
     }
 
- return (
+    return (
         <div className='main-div'>
             <h1>Reverse Polish Notation Calculator</h1>
             <div>
-                <input value={input} onInput= { e => {setInput(e.target.value)}}></input>
+                <input placeholder='Enter the operand and operator: 2,6,5.5,4,*,-,+' value={input} onInput={e => { setInput(e.target.value) }}></input>
+
                 <button onClick={clear}>Clear</button>
-            <button onClick={compute} style={{background: color, color:textColor}}>Compute</button>
+                <button onClick={compute} style={{ background: color, color: textColor }}>Compute</button>
             </div>
+
             <div className='message'>
                 {message}
             </div>
-            
+
         </div>
     )
 }
